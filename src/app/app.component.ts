dc276befa4728071d788d048cf50ee94abb53eda
Anu-cyber07 <75072999+Anu-cyber07@users.noreply.github.com>
2021-07-16 11:22:21 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
-
+import { PostService } from './post.service';
+import { details, foo} from './post';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'postalcode';
+  lst: details[]|any;
+  lst1:details | any;
+  url:any;
+
+  constructor(private apiservice:PostService) {}
+
+  ngOnInit() {}
+
+  send_details(value: any){
+    this.url = value.name;
+    this.apiservice.geturl(this.url);
+   
+    this.apiservice.getDetails()
+    .subscribe
+    (
+      data=>
+      {
+        this.lst = data;
+        this.lst1 = data;
+        console.log(data);
+        if (this.lst1.Message ==='No records found'){
+          alert('Error:No records found');
+        }
+      }
+    );
+  }
+  validatepin(y: { PostOffice: null; Message: any; }){
+    if (y.PostOffice === null){
+      return('No Records Found');
+    }
+    else {
+      return(y.Message);
+    }
+  }
 }
